bf2a0456a953800f45aa649dcaf7d631bbbcdcbe
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2025-11-13 11:53:41 +0530

    Time: 2 ms (87.35%), Space: 45.7 MB (8.67%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans =new ArrayList<>();
+        function(0,candidates,target,ans,new ArrayList<>());
+        return ans; 
+        
+    }
+    public void function(int index, int[]arr, int target, List<List<Integer>>ans,List<Integer>ds){
+        if(index==arr.length){
+            if(target==0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        if(arr[index]<=target){
+            ds.add(arr[index]);
+            function(index,arr,target-arr[index],ans,ds);
+            ds.remove(ds.size()-1);
+            
+        }
+        function(index+1,arr,target,ans,ds);
+    }
+}
