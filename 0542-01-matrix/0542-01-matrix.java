023c95ad6a53b52847e2a827907e26b511d960c7
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2025-11-26 13:58:46 +0530

    Time: 14 ms (64.56%), Space: 49.3 MB (14.56%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] result = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j]==0){
+                    result[i][j]=0;
+                    queue.add(new int[]{i,j});
+                }
+                else{
+                    result[i][j]=-1;
+
+                }
+            }
+        }
+        while(!queue.isEmpty()){
+            int[] pair = queue.poll();
+            int i = pair[0];
+            int j= pair[1];
+            if(i+1<m && result[i+1][j]==-1){
+                result[i+1][j]=result[i][j]+1;
+                queue.add(new int[]{i+1,j});
+            }
+            if(j+1<n && result[i][j+1]==-1){
+                result[i][j+1]=result[i][j]+1;
+                queue.add(new int[]{i,j+1});
+            }
+            if(i-1>=0 && result[i-1][j]==-1){
+                result[i-1][j]=result[i][j]+1;
+                queue.add(new int[]{i-1,j});
+            }
+            if(j-1>=0 && result[i][j-1]==-1){
+                result[i][j-1]=result[i][j]+1;
+                queue.add(new int[]{i,j-1});
+            }
+        }
+        return result;
+        
+    }
+}
