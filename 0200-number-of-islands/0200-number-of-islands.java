e558611913edcd0c1657d7cc3a4c9949f29aa0c1
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2025-12-07 15:26:53 +0530

    Time: 3 ms (85.64%), Space: 52.4 MB (22.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,31 +1,33 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int m = grid.length;
-        int n= grid[0].length;
-        int count=0;
-        int[] delRow={-1,0,1,0};
-        int[] delCol={0,1,0,-1};
+        int n = grid[0].length;
+        int count =0;
+        int[] delrow ={-1,0,1,0};
+        int[] delcol={0,1,0,-1};
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    dfs(grid,i,j,delRow,delCol);
+                    dfs(grid,i,j,delrow,delcol);
                 }
             }
+
         }
-        return count;   
+        return count;
     }
 
-    public void dfs(char[][]grid,int row, int col, int[]delRow, int[] delCol){
+    public void dfs(char[][] grid, int row, int col, int[] delrow, int[] delcol){
         int m = grid.length;
-        int n = grid[0].length;
+        int n= grid[0].length;
         grid[row][col]='0';
         for(int k=0; k<4; k++){
-            int nrow = row+delRow[k];
-            int ncol = col+delCol[k];
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol]=='1'){
-                dfs(grid,nrow,ncol,delRow,delCol);
+            int nr = row + delrow[k];
+            int nc = col+ delcol[k];
+            if(nr>=0 && nc>=0 && nr<m && nc<n && grid[nr][nc]=='1'){
+                dfs(grid,nr,nc,delrow,delcol);
             }
         }
+
     }
 }
