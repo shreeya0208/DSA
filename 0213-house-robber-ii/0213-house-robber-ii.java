8aa2229ebbf3bc07c01045b25221f3aba21debd4
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-01-06 11:52:34 +0530

    Time: 0 ms (100%), Space: 42.9 MB (40.59%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -4,23 +4,22 @@ class Solution {
         if(n==1)return nums[0];
         int case1 = fun(nums,0,n-2);
         int case2= fun(nums,1,n-1);
-        return Math.max(case1,case2);
+      return  Math.max(case1,case2);
+    
     }
 
     public int fun(int[]nums, int start, int end){
         int n = end-start+1;
-        int[] dp= new int[n+1];
+        int[] dp = new int[n+1];
         dp[0]=nums[start];
         for(int i=1; i<n; i++){
             int pick = nums[start+i];
             if(i>1){
-                pick=pick+dp[i-2];
+                pick+=dp[i-2];
             }
             int notpick = dp[i-1];
             dp[i]=Math.max(pick,notpick);
         }
         return dp[n-1];
-
-
     }
 }
