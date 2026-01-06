dbdb3a4850ddeab948c483bb9299a874fadb8450
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-01-06 11:37:56 +0530

    Time: 0 ms (100%), Space: 42.9 MB (18.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,17 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-    int[] dp= new int[n+1];
-    dp[0]=nums[0];
-    for(int i=1; i<n; i++){
-        int pick = nums[i];
-        if(i>1){
+        int[] dp = new int[n+1];
+        dp[0]=nums[0];
+        for(int i=1; i<n; i++){
+           int pick = nums[i];
+           if(i>1){
             pick=pick+dp[i-2];
+           }
+           int nonpick = dp[i-1];
+           dp[i]=Math.max(pick,nonpick);
         }
-        int notpick = 0+dp[i-1];
-        dp[i]=Math.max(pick,notpick);
-    }
-    return dp[n-1];
+        return dp[n-1];
         
     }
 }
