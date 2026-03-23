37866575286e8c372f0fa6c16869759968fc65ee
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-03-23 14:13:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,34 +1,33 @@
 class Solution {
     public void solve(char[][] board) {
-        int m = board.length;
-        int n = board[0].length;
-       for(int i=0; i<m; i++){
-        if(board[i][0]=='O')dfs(board,i,0);
-        if(board[i][n-1]=='O')dfs(board,i,n-1);
-       }
-       for(int j=0; j<n; j++){
-        if(board[0][j]=='O')dfs(board,0,j);
-        if(board[m-1][j]=='O')dfs(board,m-1,j);
-       }
-       for(int i=0; i<m; i++){
+        int m= board.length;
+        int n= board[0].length;
+        for(int i=0; i<m; i++){
+            if(board[i][0]=='O')dfs(board,i,0);
+            if(board[i][n-1]=='O')dfs(board,i,n-1);
+        }
         for(int j=0; j<n; j++){
-            if(board[i][j]=='S') board[i][j]='O';
-            else board[i][j]='X';
+            if(board[0][j]=='O')dfs(board,0,j);
+            if(board[n-1][j]=='O')dfs(board,n-1,j);
         }
-       }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j]=='s')board[i][j]='O';
+                else board[i][j]='X';
+            }
         }
-
-        public void dfs(char[][] board, int i, int j){
-            int m= board.length;
-            int n = board[0].length;
-            if(i<0 || j<0 || i>=m || j>=n  || board[i][j]!='O')return;
-            board[i][j]='S';
-            dfs(board,i+1,j);
-            dfs(board,i-1,j);
-            dfs(board,i,j+1);
-            dfs(board,i,j-1);
-
-        }
+      
         
     }
+    public void dfs(char[][] board, int i, int j){
+        int m= board.length;
+        int n= board[0].length;
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j]!='O')return;
+        board[i][j]='s';
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
+        dfs(board,i,j+1);
+        dfs(board,i,j-1);
 
+    }
+}
