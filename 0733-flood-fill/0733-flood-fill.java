5fbb75ca8bd579444b3ea331be0aad8992cfc6e9
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-03-23 13:18:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,22 +1,21 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int m= image.length;
-        int n= image[0].length;
-        int init = image[sr][sc];
-        if(init==color)return image;
-        dfs(image,sr,sc,color,init);
-        return image;
-    }
-
-    public void dfs(int[][] image, int sr, int sc, int color, int init){
         int m = image.length;
-        int n = image[0].length;
-        if(sr<0 || sc<0 || sr>=m || sc>=n)return;
-        if(image[sr][sc]!=init)return;
+        int n= image[0].length;
+        int initial= image[sr][sc];
+        if(image[sr][sc]==color)return image;
+        dfs(image,sr,sc,color,initial,m,n);
+        return image;
+
+
+    }
+    public void dfs(int[][] image, int sr, int sc, int color, int initial, int m, int n){
+        if(sr<0 || sr>=m || sc<0 || sc>=n)return;
+        if(image[sr][sc] != initial)return;
         image[sr][sc]=color;
-        dfs(image,sr+1,sc,color,init);
-         dfs(image,sr-1,sc,color,init);
-          dfs(image,sr,sc+1,color,init);
-           dfs(image,sr,sc-1,color,init);
+        dfs(image,sr+1,sc,color,initial,m,n);
+        dfs(image,sr-1,sc,color,initial,m,n);
+        dfs(image,sr,sc+1,color,initial,m,n);
+        dfs(image,sr,sc-1,color,initial,m,n);
     }
 }
