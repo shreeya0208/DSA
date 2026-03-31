42acfaaa474feadc862537b57dd57271e430687a
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-03-31 11:55:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -10,13 +10,11 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if(head==null || head.next==null){
-            return null;
-        }
+        if(head==null || head.next==null)return null;
         ListNode fast= head;
-        ListNode slow = head;
+        ListNode slow= head;
         fast=fast.next.next;
-       while(fast!=null && fast.next!=null){
+        while(fast!=null && fast.next!=null){
             fast=fast.next.next;
             slow=slow.next;
         }
@@ -24,5 +22,4 @@ class Solution {
         slow.next=slow.next.next;
         return head;
     }
-
 }
