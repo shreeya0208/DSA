d12d36cf7cd6d29a48853658a03bb5c192e92b46
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-04-01 17:05:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,26 +1,16 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head==null || head.next==null)return head;
-        ListNode mid = mid(head);
-        ListNode rightHead = mid.next;
-        mid.next=null;
-        ListNode left= sortList(head);
-        ListNode right=sortList(rightHead);
-        return merge(left,right);
+      if(head==null || head.next==null)return head;  
+      ListNode mid = middle(head);
+      ListNode rightHead=mid.next;
+      mid.next=null;
+      ListNode left = sortList(head);
+      ListNode right=sortList(rightHead);
+      return merge(left,right);
     }
     public ListNode merge(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
+        ListNode temp=dummy;
         while(l1!=null && l2!=null){
         if(l1.val < l2.val){
             temp.next=l1;
@@ -32,15 +22,13 @@ class Solution {
         }
         temp=temp.next;
         }
-        if(l1 != null) temp.next=l1;
+        if(l1!=null)temp.next=l1;
         if(l2!=null)temp.next=l2;
         return dummy.next;
-
-    
     }
-    public ListNode mid(ListNode head){
-        ListNode fast=  head;
-        ListNode slow = head;
+    public ListNode middle(ListNode head){
+        ListNode fast=head;
+        ListNode slow=head;
         while(fast.next!=null && fast.next.next!=null){
             fast=fast.next.next;
             slow=slow.next;
