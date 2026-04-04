25e75a3e697bbdd43ea44a3adb38cf1d536db2ef
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-04-04 22:32:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        int[] taken= new int[nums.length];
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> current= new ArrayList<>();
-        Arrays.sort(nums);
-        helper(taken,nums,ans,current);
-        return ans;
+        int[] taken = new int[nums.length];
+        List<List<Integer>> result= new ArrayList<>();
+        List<Integer> ls = new ArrayList<>();
+        helper(nums,taken,result,ls);
+        return result;
+
     }
-    public void helper(int[] taken, int[] nums, List<List<Integer>> ans, List<Integer> current){
-        if(current.size()==nums.length){
-            ans.add(new ArrayList<>(current));
+    public void helper(int[]nums, int[] taken, List<List<Integer>> result, List<Integer> ls){
+        if(ls.size()==nums.length){
+            result.add(new ArrayList<>(ls));
             return;
         }
         for(int i=0; i<nums.length; i++){
             if(taken[i]==1)continue;
             if(i>0 && nums[i]==nums[i-1] && taken[i-1]==0)continue;
             taken[i]=1;
-            current.add(nums[i]);
-            helper(taken,nums,ans,current);
-            current.remove(current.size()-1);
+            ls.add(nums[i]);
+            helper(nums,taken,result,ls);
+            ls.remove(ls.size()-1);
             taken[i]=0;
         }
     }
