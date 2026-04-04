75041f3274eb2c61671e3013bc9bb1104f00d2e4
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-04-04 13:40:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,24 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
         List<List<Integer>> ans = new ArrayList<>();
-        backtrack(0, candidates, target, ans, new ArrayList<>());
+        List<Integer> ls = new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(candidates,target,ans,ls,0);
         return ans;
     }
-
-    private void backtrack(int start, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
-        if (target == 0) {
-            ans.add(new ArrayList<>(ds));
+    public void helper(int[]arr, int target,List<List<Integer>> ans,  List<Integer> ls, int index ){
+        if(target==0){
+            ans.add(new ArrayList<>(ls));
             return;
         }
-
-        for (int i = start; i < arr.length; i++) {
-            if (i > start && arr[i] == arr[i - 1]) continue;
-            if (arr[i] > target) break;
-            ds.add(arr[i]);
-            backtrack(i + 1, arr, target - arr[i], ans, ds);
-            ds.remove(ds.size() - 1);
+        for(int i=index; i<arr.length; i++){
+            if(i>index && arr[i]==arr[i-1])continue;
+            if(arr[i]>target)break;
+            ls.add(arr[i]);
+            helper(arr,target-arr[i],ans,ls, i+1);
+            ls.remove(ls.size()-1);
         }
-    }
 }
-    
+}
