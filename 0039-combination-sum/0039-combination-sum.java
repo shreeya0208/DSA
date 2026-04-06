821ea5827e1cc0030a2bf8654ffeaa2f6b3eb16b
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-04-06 20:21:35 +0530

    Time: 2 ms (95.59%), Space: 45.8 MB (40.91%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -6,18 +6,18 @@ class Solution {
         return ans;
     }
     public void helper(int[] candidates, int target, List<List<Integer>> ans, List<Integer> ls, int index ){
-        if(candidates.length==index){
-        if(target==0){
+        if(candidates.length == index){
+            if(target==0){
             ans.add(new ArrayList<>(ls));
+            }
+            return;
         }
-             return;
-        }
-        if(candidates[index]<=target){
-            ls.add(candidates[index]);
-            helper(candidates,target-candidates[index],ans, ls, index);
-            ls.remove(ls.size()-1);
-        }
-        helper(candidates,target,ans, ls, index+1);
+                if(candidates[index]<=target){
+                    ls.add(candidates[index]);
+                    helper(candidates,target-candidates[index],ans,ls,index);
+                    ls.remove(ls.size()-1);
+                }
+              helper(candidates,target,ans,ls,index+1);
 
     }
 }
