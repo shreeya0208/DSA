185ef97e16c552bcb2f623ea2ce019cb541650e6
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-04-06 19:02:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,34 +1,34 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int start=0;
-        int end = max(piles);
+        int end=max(piles);
         while(start<=end){
-            int mid = start+(end-start)/2;
-            int total = totalHours(piles,mid);
-            if(total<=h){
-                end=mid-1;
-            }
+            int mid= start+(end-start)/2;
+            int total= totalHours(piles,mid);
+            if(h>=total)end=mid-1;
             else{
                 start=mid+1;
             }
         }
         return start;
+        
     }
     public int max(int[] piles){
-        int max= Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
         for(int i=0; i<piles.length; i++){
             if(piles[i]>max){
-            max=piles[i];
+                max=piles[i];
             }
+
         }
         return max;
     }
+
     public int totalHours(int[] piles, int mid){
         int total=0;
         for(int i=0; i<piles.length; i++){
             total+=Math.ceil((double)piles[i]/(double)mid);
         }
-         return total;
+        return total;
     }
-   
 }
