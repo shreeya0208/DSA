a86493cf047c2ea8115ae030806093520ce61644
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-05-24 23:42:19 +0530

    Time: 0 ms (100%), Space: 47.7 MB (67.6%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -6,23 +6,25 @@ class Solution {
         int left=0;
         int right=height.length-1;
         while(left<right){
-            if(height[left]<=height[right]){
-                if(leftmax>height[left]){
-                    total += leftmax-height[left];
-                }
-                else{
-                    leftmax=height[left];
-                }
-                left++;
-            }
-            else{
-                    if(rightmax>height[right]){
-                        total+= rightmax-height[right];
+            if(height[left]<= height[right]){
+                    if(leftmax>height[left]){
+                        total=total+leftmax-height[left];
                     }
                     else{
-                        rightmax=height[right];
+
+                        leftmax=height[left];
+
                     }
-                    right--;
+                    left++;
+            }
+            else{
+                if(rightmax>height[right]){
+        total=total+rightmax-height[right];                }
+                else{
+                    rightmax=height[right];
+                }
+                right--;
+
             }
         }
         return total;
