23badcd606ef00c62c5100f0c6e7945e59557aac
Shreeya <157449634+shreeya0208@users.noreply.github.com>
2026-05-29 17:45:49 +0530

    Time: 178 ms (5.16%), Space: 49.3 MB (31.71%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,51 +1,52 @@
 class Pair{
     int first;
     int second;
-    Pair(int first, int second){
+    Pair(int first , int second){
         this.first=first;
         this.second=second;
     }
 }
+
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int m= mat.length;
+        Queue<Pair>queue = new LinkedList<>();
+        int m =mat.length;
         int n= mat[0].length;
-     Queue<Pair> queue= new LinkedList<>();  
-      int[][] result= new int[m][n];
-      for(int i=0; i<m; i++){
-        for(int j=0; j<n; j++){
-            if(mat[i][j]==0){
-                result[i][j]=0;
-                queue.add(new Pair(i,j));
-            }
-            else {
-                result[i][j]=-1;
+        int[][] result= new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j]==0){
+                    result[i][j]=0;
+                    queue.add(new Pair(i,j));
+                }
+                else{
+                    result[i][j]=-1;
+                }
             }
         }
-      }
-      while(!queue.isEmpty()){
-        Pair p= queue.poll();
-        int row= p.first;
-        int col= p.second;
-        if(row-1>=0 && result[row-1][col]==-1){
-            result[row-1][col]=result[row][col]+1;
-            queue.add(new Pair(row-1,col));
+        while(!queue.isEmpty()){
+            Pair p = queue.poll();
+            int row = p.first;
+            int col = p.second;
+            if(row-1>=0 && result[row-1][col]==-1){
+                result[row-1][col]=result[row][col]+1;
+                queue.add(new Pair(row-1,col));
+
+            }
+            if(row+1<m && result[row+1][col]==-1){
+                result[row+1][col]=result[row][col]+1;
+                queue.add(new Pair(row+1,col));
+            }
+            if(col-1>=0 && result[row][col-1]==-1){
+                result[row][col-1]=result[row][col]+1;
+                queue.add(new Pair(row,col-1));
+            }
+            if(col+1<n && result[row][col+1]==-1){
+                result[row][col+1]=result[row][col]+1;
+                queue.add(new Pair(row,col+1));
+            }
 
         }
-        if(row+1<m && result[row+1][col]==-1){
-            result[row+1][col]=result[row][col]+1;
-            queue.add(new Pair(row+1,col));
-        }
-        if(col-1>=0 && result[row][col-1]==-1){
-            result[row][col-1]=result[row][col]+1;
-            queue.add(new Pair(row,col-1));
-        }
-        if(col+1<n && result[row][col+1]==-1){
-            result[row][col+1]=result[row][col]+1;
-            queue.add(new Pair(row,col+1));
-        }
-      }
-      return result;
+        return result;
     }
-    
 }
